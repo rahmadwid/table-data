cdcba1c24f361592647a279445fd5b9a8595b6f3
rahmadwid <rahmadwidayat26@gmail.com>
2022-04-07 15:41:07 +0700

    edit home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,9 @@ function Home() {
                 <Col md="{12}">
                     <Card className="border-0 rounded shadow-sm">
                         <Card.Body className="p-4">
-                        <h1>EXPRESS.JS + VUE.JS</h1>
-                        <p class="lead">Tutorial FullStack Express.js dan React.js oleh <strong>SantriKoding.com</strong></p>
-                        <Button href="http://santrikoding.com" target="_blank" variant="primary" size="lg">SELENGKAPNYA</Button>
+                        <h1>EXPRESS.JS + REACTJS</h1>
+                        <p class="lead">Fitur mengelola data Express.js dan React.js oleh <strong>Rahmad Widayat</strong></p>
+                        
                         </Card.Body>
                     </Card>
                 </Col>
